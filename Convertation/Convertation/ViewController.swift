7274c8719e08ec3d8ca4f6072697fb30d6d9d1f1
jompan <jolkro@hotmail.com>
2016-04-18 22:02:07 +0200

    vecka 3 forts

--- Convertation/Convertation/ViewController.swift
+++ Convertation/Convertation/ViewController.swift
@@ -12,13 +12,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     var tempUnits : NSDictionary!
     var lengthUnits1 : NSDictionary!
-    var units2 : NSDictionary!
-    var units3 : NSDictionary!
-
+    var weightUnits2 : NSDictionary!
+    var timeUnits3 : NSDictionary!
+    var heatUnits4 : NSDictionary!
+    var speedUnits5 : NSDictionary!
+    var bitUnits6 : NSDictionary!
+    
     var tempKeys : NSArray!
     var lengthKeys1 : NSArray!
-    var keys2 : NSArray!
-    var keys3 : NSArray!
+    var weightKeys2 : NSArray!
+    var timeKeys3 : NSArray!
+    var heatKeys4 : NSArray!
+    var speedKeys5 : NSArray!
+    var bitKeys6 : NSArray!
+
+
     
 
     @IBOutlet weak var txtAmount: UITextField!
@@ -35,21 +43,48 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         //2.get a reference to the path of the file that contains the unit
         let filePath1 = appBundle.pathForResource("lengthList", ofType: "plist")!
         let filePath2 = appBundle.pathForResource("weightList", ofType: "plist")!
+        let filePath3 = appBundle.pathForResource("timeList", ofType: "plist")!
+        let filePath4 = appBundle.pathForResource("heatList", ofType: "plist")!
+        let filePath5 = appBundle.pathForResource("speedList", ofType: "plist")!
+        let filePath6 = appBundle.pathForResource("bitList", ofType: "plist")!
+
+
+
 
         //load the contents of the file into the dictionary
         tempUnits = NSDictionary(contentsOfFile: filePath1)
         lengthUnits1 = NSDictionary(contentsOfFile: filePath1)
-        units2 = NSDictionary(contentsOfFile: filePath2)
+        weightUnits2 = NSDictionary(contentsOfFile: filePath2)
+        timeUnits3 = NSDictionary(contentsOfFile: filePath3)
+        heatUnits4 = NSDictionary(contentsOfFile: filePath4)
+        speedUnits5 = NSDictionary(contentsOfFile: filePath5)
+        bitUnits6 = NSDictionary(contentsOfFile: filePath6)
+
+
+
         
         //Initiailze the keys with the names of the units
         tempKeys = lengthUnits1.allKeys as NSArray
         lengthKeys1 = lengthUnits1.allKeys as NSArray
-        keys2 = units2.allKeys as NSArray
+        weightKeys2 = weightUnits2.allKeys as NSArray
+        timeKeys3 = timeUnits3.allKeys as NSArray
+        heatKeys4 = heatUnits4.allKeys as NSArray
+        speedKeys5 = speedUnits5.allKeys as NSArray
+        bitKeys6 = bitUnits6.allKeys as NSArray
+
+
+
 
         //Sort the keys in alphabetical in ascending order
         tempKeys = lengthKeys1.sort(sortKeys)
         lengthKeys1 = lengthKeys1.sort(sortKeys)
-        keys2 = keys2.sort(sortKeys)
+        weightKeys2 = weightKeys2.sort(sortKeys)
+        timeKeys3 = timeKeys3.sort(sortKeys)
+        heatKeys4 = heatKeys4.sort(sortKeys)
+        speedKeys5 = speedKeys5.sort(sortKeys)
+        bitKeys6 = bitKeys6.sort(sortKeys)
+
+
 
         
         
@@ -124,14 +159,55 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     @IBAction func weightButton(sender: AnyObject) {
-        tempKeys = keys2
-        tempUnits = units2
-        pickerLength = keys2.count
+        tempKeys = weightKeys2
+        tempUnits = weightUnits2
+        pickerLength = weightKeys2.count
         
         
         [self.pvUnitTo .reloadAllComponents()]
         [self.pvUnitFrom .reloadAllComponents()]
     }
 
+    @IBAction func timeButton(sender: AnyObject) {
+        
+        tempKeys = timeKeys3
+        tempUnits = timeUnits3
+        pickerLength = timeKeys3.count
+        
+        
+        [self.pvUnitTo .reloadAllComponents()]
+        [self.pvUnitFrom .reloadAllComponents()]
+    }
+    @IBAction func heatButton(sender: AnyObject) {
+        tempKeys = heatKeys4
+        tempUnits = heatUnits4
+        pickerLength = heatKeys4.count
+        
+        
+        [self.pvUnitTo .reloadAllComponents()]
+        [self.pvUnitFrom .reloadAllComponents()]
+
+    }
+    
+    @IBAction func speedButton(sender: AnyObject) {
+        tempKeys = speedKeys5
+        tempUnits = speedUnits5
+        pickerLength = speedKeys5.count
+        
+        
+        [self.pvUnitTo .reloadAllComponents()]
+        [self.pvUnitFrom .reloadAllComponents()]
+    }
+    
+    @IBAction func bitButton(sender: AnyObject) {
+        tempKeys = bitKeys6
+        tempUnits = bitUnits6
+        pickerLength = bitKeys6.count
+        
+        
+        [self.pvUnitTo .reloadAllComponents()]
+        [self.pvUnitFrom .reloadAllComponents()]
+    }
+    
 }
 
